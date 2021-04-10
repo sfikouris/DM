f1979d9d2a3403a38d1b3fb7f74a6e82cde57114
Paris <parissf15@hotmail.com>
2021-04-10 21:47:42 +0200

    a

--- task1/task1.py
+++ task1/task1.py
@@ -7,6 +7,24 @@ df = pd.read_csv('ODI-2021.csv')
 
 #cleaning 
 df['When is your birthday (date)?'] = pd.to_datetime(df['When is your birthday (date)?'], errors='coerce')
+df['Time you went to be Yesterday'] = pd.to_datetime(df['Time you went to be Yesterday'], errors='coerce')
+
+print(df['Time you went to be Yesterday'].dt.time.to_string())
+
+#make the values of column 1 same
+df['What programme are you in?'] = df['What programme are you in?'].replace(
+    to_replace = ["ai","AI","Ai","AI (uva)","AI Masters",
+    "AI: cognitive science track","artificial intelligence","Artificial Intellingence",
+    "MSc AI","Master Artificial Intelligence","Master AI at UvA","Artificial Intelligence","Msc Artificial Intelligence ",
+    "artificial intelligence","Artificial Intelligence Masters","Artificial Intelligence MSc","MSc: Artificial Intelligence",
+    "Master Artificial Intelligence: Cognitive Sciences ","Masters of Artificial Intelligence","Msc AI","MSc Artificial Intelligence",
+    "Msc Artificial Intelligence","MSc Artificial Intelligence @UvA","Artificial intelligence"],
+    value = "Artificial Intelligence")
+
+df['What programme are you in?'] = df['What programme are you in?'].replace(
+    to_replace = ["BA","Business analytics ","Business Analytics & AI","Master Business Analytics"],
+    value = "Business Analytics")
+
 
 #cleaning 'what is your stress level (0-100)?' column.
 counter = 0
@@ -37,7 +55,22 @@ for row in df['Number of neighbors sitting around you?']:
         df.loc[counter, 'Number of neighbors sitting around you?']=np.nan
     counter+=1
 
-print(df['Number of neighbors sitting around you?'].to_string())
+
+# cleaning You can get 100 euros if you win a local DM competition, or we don’t hold
+#  any competitions and I give everyone some money (not the same amount!). How much do you think you would deserve then? 
+counter = 0 
+for row in df['You can get 100 euros if you win a local DM competition, or we don’t hold any competitions and I give everyone some money (not the same amount!). How much do you think you would deserve then? ']:
+    try:
+        row = float(row)
+        if row > 100:
+            df.loc[counter,'You can get 100 euros if you win a local DM competition, or we don’t hold any competitions and I give everyone some money (not the same amount!). How much do you think you would deserve then? ']=100
+        elif row < 0:
+            df.loc[counter,'You can get 100 euros if you win a local DM competition, or we don’t hold any competitions and I give everyone some money (not the same amount!). How much do you think you would deserve then? ']=0
+    except ValueError:
+        df.loc[counter,'You can get 100 euros if you win a local DM competition, or we don’t hold any competitions and I give everyone some money (not the same amount!). How much do you think you would deserve then? '] =np.nan
+    counter+=1
+
+
 #fixing types
 
 #df['Have you taken a course on machine learning?'] = df['Have you taken a course on machine learning?'].astype('bool')
