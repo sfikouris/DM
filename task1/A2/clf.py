c39f3dae220ab970ef1310a6ec02796b1b7d8bc3
Paris <parissf15@hotmail.com>
2021-04-14 19:01:52 +0200

    forest

--- task1/A2/clf.py
+++ task1/A2/clf.py
@@ -1,7 +1,9 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
 import sklearn as sk
 from sklearn import datasets
 from sklearn import svm
@@ -10,46 +12,56 @@ import pandas as pd
 
 df = pd.read_csv('heart.csv', sep= ',', header =0)
 
-
+#cleaning dataset
 df.loc[df['famhist']=='Present','famhist'] = 1
 df.loc[df['famhist']=='Absent','famhist'] = 0
 
-#print(df.head())
-#y = df.iloc[:,9]
+#create data 
 y = df['chd']
-#X = df.iloc[:,:9]
 X = df.drop('chd',axis=1)
 
+#preparing data for train
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=0)
 
+#apply svm model with cross-valitation and train the algorithm
 clf = svm.SVC(kernel='linear',C=1).fit(X_train, y_train)
-clf_with_CV = svm.SVC(kernel='linear',C=1,random_state=40).fit(X_train, y_train)
-scores = cross_val_score(clf_with_CV, X_test,y_test,cv=5)
+clf_with_CV = svm.SVC(kernel='linear',C=1,random_state=42).fit(X_train, y_train)
 
 #making prediction
 y_pred = clf_with_CV.predict(X_test)
 #print(y_pred)
 
 #evaluating the Algorithm
+scores = cross_val_score(clf_with_CV, X_test,y_test,cv=5)
 print(confusion_matrix(y_test,y_pred))
 print(classification_report(y_test,y_pred))
+print(accuracy_score(y_test, y_pred))
 
 
 print(scores)
 print("%0.2f accuracy with a standard deviation of %0.2f" %(scores.mean(), scores.std()))
 #print(clf.score(X_train, y_train))
 
-
 #print("PRININT Y")
 #print(y)
 #print("prinintg X")
 #print(X.to_string())
 
+#Random Forest for Regression
+
+#training the algorithm
+regressor = RandomForestClassifier(max_depth=2, random_state=42).fit(X_train,y_train)
+y_pred_forest = regressor.predict(X_test)
+scores_forest = cross_val_score(regressor, X_test,y_test,cv=5)
+
+
+print(scores_forest)
+print("%0.2f accuracy with a standard deviation of %0.2f" %(scores_forest.mean(), scores_forest.std()))
+
+print("FOREST")
+print(confusion_matrix(y_test,y_pred_forest))
+print(classification_report(y_test,y_pred_forest))
+print(accuracy_score(y_test, y_pred_forest))
 
 
 
-#SVM = svm.LinearSVC()
-#print(SVM)
-#SVM.fit(X,y)
-#SVM.predict(X.iloc[460:,:])
-#round(SVM.score(X,y),4)
